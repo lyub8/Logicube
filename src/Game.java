422a50d49647596d573a1ca9b54abfebe0f972d1
Lyubomir Ivanov <lyubomir.ivanov.2015@uni.strath.ac.uk>
2017-12-11 22:38:40 +0000

    interface building

--- src/Game.java
+++ src/Game.java
@@ -3,15 +3,19 @@ import java.util.Random;
 import javafx.application.Application;
 import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
+import javafx.geometry.Insets;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.GridPane;
+import javafx.scene.layout.HBox;
+import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
 
 public class Game extends Application {
 	private GridPane root;
 	private Button[][] grid;
-	private int size = 5;
+	private int size = 15;
 	private Stage stage;
 	private int[][] matrix;
 	
@@ -33,7 +37,7 @@ public class Game extends Application {
 	    	for(int x=0; x<size; x++) {
 	                Random rand = new Random(); 
 	                
-	                grid[x][y] = new Button();   
+	                grid[x][y] = new Button(); 
 	                grid[x][y].setText(String.valueOf(matrix[x][y])); 
 	                grid[x][y].setOnAction(new EventHandler<ActionEvent>() {
 	                    @Override
@@ -45,9 +49,32 @@ public class Game extends Application {
 	            }
 	    }        
 
-	    Scene scene = new Scene(root, 500, 500);
-	    stage.setTitle("Random Binary Matrix (JavaFX)");
+	    BorderPane border = new BorderPane();
+	    border.setStyle("-fx-background-color: #9acd32;");
+	    border.setTop(addHBox());
+	    border.setBottom(addHBox());
+	    border.setLeft(addVBox()); 
+	    border.setRight(addVBox()); 
+	    border.setCenter(root);
+	    root.setPadding(new Insets(100));
+	    
+	    Scene scene = new Scene(border);
+	    stage.setTitle("Logicum");
 	    stage.setScene(scene);
 	    stage.show();
+	}
+	
+	private HBox addHBox() {
+		HBox hbox = new HBox();
+	    hbox.setStyle("-fx-background-color: #6495ed;");
+	    hbox.setPrefSize(500, 100);
+	    return hbox;
+	}
+	
+	private VBox addVBox() {
+		VBox vbox = new VBox();
+	    vbox.setStyle("-fx-background-color: #6495ed;");
+	    vbox.setPrefSize(100, 300);
+	    return vbox;
 	}
 }
